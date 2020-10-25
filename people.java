702820568088f91c611c2e8b8cd95b20f88327db
Li-YiLin <71876479+Li-YiLin@users.noreply.github.com>
2020-10-25 10:45:17 +0800

    Add files via upload

---
+++ people.java
@@ -0,0 +1,15 @@
+package Homework2;
+
+public class people {
+      int bianhao;
+      String name;
+      String sex;
+      people (int bianhao,String name,String sex){
+            this.bianhao = bianhao;
+            this.name = name;
+            this.sex = sex;
+      }
+      people(){
+
+      }
+}
